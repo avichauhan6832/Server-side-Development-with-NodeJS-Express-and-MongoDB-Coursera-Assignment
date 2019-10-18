7270216f019318b9c8df70d7f6eb5293d756b910
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2019-10-18 17:42:52 +0530

    File Upload

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ var leaderRouter = require('./routes/leaderRouter');
 var promoRouter = require('./routes/promoRouter');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var uploadRouter = require('./routes/uploadRouter');
 
 const Dishes = require('./models/dishes');
 
@@ -65,6 +66,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload',uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
