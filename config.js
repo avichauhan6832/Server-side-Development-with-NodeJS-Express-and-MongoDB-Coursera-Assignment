2cd507baa1ec0a0a4f01504d8b97edbc8716bc49
avichauhan6832 <avi.r.chauhan6832@gmail.com>
2019-10-21 13:00:16 +0530

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId : '2653632441409544',
+        clientSecret : '18ccf39a627e0f9756eaf688fe35f0e2'
+    }
 }
